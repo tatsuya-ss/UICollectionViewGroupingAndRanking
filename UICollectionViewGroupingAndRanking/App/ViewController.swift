5d80526e096d7e35d6ea4fb4f94232b62580aba8
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-02-20 16:05:30 +0900

    都道府県のモデルを作成

--- UICollectionViewGroupingAndRanking/App/ViewController.swift
+++ UICollectionViewGroupingAndRanking/App/ViewController.swift
@@ -7,6 +7,22 @@
 
 import UIKit
 
+struct Prefecture: Hashable {
+    let name: String
+    let localType: LocalType = .Hokkaido
+}
+
+enum LocalType {
+    case Hokkaido, Tohoku, Kanto, Chubu, Kinki, Chugoku, Shikoku, Kyushu, Okinawa
+}
+
+struct Prefectures {
+    let prefectureNames = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"]
+    var prefectures: [Prefecture] {
+        prefectureNames.map { Prefecture(name: $0)}
+    }
+}
+
 final class ViewController: UIViewController {
     
     @IBOutlet private weak var collectionView: UICollectionView!
@@ -15,8 +31,8 @@ final class ViewController: UIViewController {
         case main
     }
     
-    private var dataSource: UICollectionViewDiffableDataSource<Section, String>! = nil
-    private let prefectures = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"]
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Prefecture>! = nil
+    private let prefectures = Prefectures().prefectures
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,14 +48,14 @@ final class ViewController: UIViewController {
 extension ViewController {
     
     private func initialDataSource() {
-        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Prefecture>()
         snapshot.appendSections([.main])
         snapshot.appendItems(prefectures, toSection: .main)
         dataSource.apply(snapshot, animatingDifferences: true)
     }
     
     private func configureDataSource() {
-        dataSource = UICollectionViewDiffableDataSource<Section, String>(
+        dataSource = UICollectionViewDiffableDataSource<Section, Prefecture>(
             collectionView: collectionView,
             cellProvider: { collectionView, indexPath, itemIdentifier in
                 guard let cell = collectionView.dequeueReusableCell(
@@ -47,7 +63,7 @@ extension ViewController {
                     for: indexPath
                 ) as? CollectionViewCell
                 else { fatalError("CollectionViewCellが見つかりませんでした。") }
-                cell.configure(title: itemIdentifier)
+                cell.configure(title: itemIdentifier.name)
                 return cell
             })
     }
